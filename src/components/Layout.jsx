f322fc1437bdb51a2f1d2232a5729cccfcd7d72b
Inteyaz <inteyazansari322@gmail.com>
2026-09-07 10:41:35 +0530

    udpated sidebar and layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
 function Layout({ children }) {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   return (
     <div className="min-h-screen bg-slate-100">
-      <Sidebar />
+      <Sidebar mobileOpen={mobileOpen} setMobileOpen={setMobileOpen} />
 
       <div className="lg:ml-64">
-        <Navbar />
+        <Navbar onMenuClick={() => setMobileOpen(true)} />
 
         <main className="p-4 sm:p-6 lg:p-8">{children}</main>
       </div>
