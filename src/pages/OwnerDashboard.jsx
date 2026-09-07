5a569725baa5e7acec6e263ce997a82d662e2b9d
Inteyaz <inteyazansari322@gmail.com>
2026-09-07 11:55:09 +0530

    updates

--- src/pages/OwnerDashboard.jsx
+++ src/pages/OwnerDashboard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { projects } from "../projects/projectdata";
 import { sites } from "../sites/siteData";
-import { tasks } from "../tasks/taskData";
+import { tasks } from "../tasks/taskData.js";
 import { inventory } from "../inventory/inventoryData";
 import { projectFinance } from "../finance/financeData";
 
