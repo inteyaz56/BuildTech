5dd8a0825265737857034fada8b938fb8f653f10
Inteyaz <inteyazansari322@gmail.com>
2026-09-07 10:48:21 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import { useSelector } from "react-redux";
 
 import Layout from "./components/Layout";
+
 import OwnerDashboard from "./pages/OwnerDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import ProjectManagerDashboard from "./pages/ProjectManagerDashboard";
+import SiteManagerDashboard from "./pages/SiteManagerDashboard";
+import AccountantDashboard from "./pages/AccountantDashboard";
+import ProcurementDashboard from "./pages/ProcurementDashboard";
+import SupervisorDashboard from "./pages/SupervisorDashboard";
+import ContractorDashboard from "./pages/ContractorDashboard";
 
 function App() {
   const currentUser = useSelector((state) => state.user.currentUser);
 
-  return <Layout>{currentUser.role === "owner" && <OwnerDashboard />}</Layout>;
+  const renderDashboard = () => {
+    switch (currentUser.role) {
+      case "owner":
+        return <OwnerDashboard />;
+
+      case "admin":
+        return <AdminDashboard />;
+
+      case "project_manager":
+        return <ProjectManagerDashboard />;
+
+      case "site_manager":
+        return <SiteManagerDashboard />;
+
+      case "accountant":
+        return <AccountantDashboard />;
+
+      case "procurement_manager":
+        return <ProcurementDashboard />;
+
+      case "supervisor":
+        return <SupervisorDashboard />;
+
+      case "contractor":
+        return <ContractorDashboard />;
+
+      default:
+        return <OwnerDashboard />;
+    }
+  };
+
+  return <Layout>{renderDashboard()}</Layout>;
 }
 
 export default App;
